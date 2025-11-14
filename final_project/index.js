a2e7c29850274e892aa5b04a853ddb08a6480bd1
ElmalkiHamza <hamza.elmalki.pc@gmail.com>
2025-11-14 19:58:50 +0100

    implementing authentication mechanisme

--- final_project/index.js
+++ final_project/index.js
@@ -6,17 +6,19 @@ const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if(!req.session.authorization){
+        return res.status(403).json({message: "Unauthorized access"});
+    } 
+    next();
 });
  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
-
 app.listen(PORT,()=>console.log("Server is running"));
